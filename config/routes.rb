c676a7299dc2d8f139a596c75eb3d5a864227890
KURUMI-HARAPECO <kurumilk509@gmail.com>
2021-05-05 03:45:07 +0000

    課題５完成

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'homes#top'
   get 'home/about' => 'homes#about'
+  get 'search' => 'search#search'
   resources :users,only: [:show,:index,:edit,:update]
   resources :books
 end
